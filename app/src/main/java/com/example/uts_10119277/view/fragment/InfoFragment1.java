89c34b9c5d6f176a3d69cd067caf3395906de53c
Marsa19 <muhamadmarsa01@gmail.com>
2022-06-07 13:44:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uts_10119277/view/fragment/InfoFragment1.java
@@ -0,0 +1,21 @@
+package com.example.uts_10119277.view.fragment;
+//10119277-Muhamad Marsa-IF7
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.uts_10119277.R;
+
+public class InfoFragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_info_slide1, container, false);
+        return rootView;
+    }
+}
